5fba276be48b8ab4562bb58a3b247a98ba410927
Rawat Aman <2402031000053@silveroakuni.ac.in>
2026-06-11 21:15:06 +0530

    Added new file and Eddited old file

---
+++ React All/src/APIcalling.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+function ApIcalling() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  return (
+    <div>
+      <h1> User Data Table </h1>
+      <table border="1" cellPadding="10">
+        <thead>
+          <tr>
+            <th> ID </th>
+            <th> Name </th>
+            <th> Email </th>
+            <th> City </th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            users.map((user) => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.address.city}</td>
+              </tr>
+
+            ))
+          }
+        </tbody>
+      </table>
+    </div>
+  );
+}
+export default ApIcalling;
